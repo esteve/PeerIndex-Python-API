d8077e59e0a310feeffd98e3abe11fec7dbd7e05
Esteve Fernandez <esteve.fernandez@gmail.com>
2012-03-30 12:22:52 +0200

    added setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+setupdict= {
+    'name': 'peerindex',
+    'version': '0.1',
+    'author': 'Guido van Oorschot',
+    'url': 'http://dev.peerindex.com/',
+    'description': 'A Python interface for the PeerIndex API.'
+    'long_description': 'A Python interface for the PeerIndex API.'
+    'py_modules': ['PeerIndexPy'],
+    }
+
+try:
+    from setuptools import setup
+except ImportError:
+    from distutils.core import setup
+
+setup(**setupdict)
